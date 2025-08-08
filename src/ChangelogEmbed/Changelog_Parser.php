47769b626c5da81b65aebc7603ebb9fad73eb3b0
Eric Defore <eric.defore@gmail.com>
2025-08-08 14:05:25 -0400

    Fix code blocks in changelogs
    These now render properly. Additionally, existing shortcodes no longer
    attempt to render on the frontend if they are properly within backticks
    in the changelog.

--- src/ChangelogEmbed/Changelog_Parser.php
+++ src/ChangelogEmbed/Changelog_Parser.php
@@ -72,6 +72,33 @@ class Changelog_Parser {
 				$type    = trim( $matches[1] );
 				$content = trim( $matches[2] );
 
+				// Wrap text in backticks with <code> tags and double-escape shortcodes.
+				$content = preg_replace_callback(
+					'/`([^`]+)`/',
+					function ( $matches ) {
+						$code_content = $matches[1];
+
+						global $shortcode_tags;
+
+						$registered_shortcodes = array_map(
+							function ( $tag ) {
+								return preg_quote( $tag, '/' );
+							},
+							array_keys( $shortcode_tags )
+						);
+
+						// Escape shortcodes that exist on this site by adding extra square brackets.
+						$code_content = preg_replace(
+							'/\[(' . implode( '|', $registered_shortcodes ) . ')\]/',
+							'[[$1]]',
+							$code_content
+						);
+
+						return '<code>' . esc_html( $code_content ) . '</code>';
+					},
+					$content
+				);
+
 				$change = [
 					'type'    => $type,
 					'content' => $content,
